a66c108e086bb1bd1bcf2ca4ab83c1c9ecf28ff3
nenew <nenew.net@gmail.com>
2016-11-21 22:55:36 +0800

    add while true loop

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-./kcptunclient -r $server_ip:$server_port -l :10000 &
-sslocal -s localhost -p 10000 -b 0.0.0.0 -l 6606 -k $server_pw -m $server_methord &
+
+while true; do
+    pid1=`ps -ef|grep sslocal|grep -v grep|awk '{print $2}'`
+	  pid2=`ps -ef|grep kcptunclient|grep -v grep|awk '{print $2}'`
+    if [ -z "$pid1" ]; then
+		./kcptunclient -r $server_ip:$server_port -l :10000 &
+		if [ -z "$pid2" ];then
+			sslocal -s localhost -p 10000 -b 0.0.0.0 -l 6606 -k $server_pw -m $server_methord &
+		fi
+    else
+        sleep 60
+    fi
+done
