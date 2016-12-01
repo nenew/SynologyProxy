b87be40a218e957fdd184cd6a4111f44b9611b2a
nenew <nenew.net@gmail.com>
2016-12-01 20:27:20 +0800

    Update runkcp.sh

--- runkcp.sh
+++ runkcp.sh
@@ -9,6 +9,7 @@ while true; do
 		if [ "$num" ];then
 			errorcondition1=`cat /var/log/kcptun.log | grep scavenged`
 			errorcondition2=`cat /var/log/kcptun.log | grep exit`
+			errorcondition3=`cat /var/log/kcptun.log | grep broken`			
 			if [ "$errorcondition1" ];then
 				echo $num
 				date >> /var/log/kcptun_restart.log
@@ -17,6 +18,13 @@ while true; do
 				echo new > /var/log/kcptun.log
 			fi
 			if [ "$errorcondition2" ];then
+				echo $num
+				date >> /var/log/kcptun_restart.log
+				echo -n "  restarted server" >> /var/log/kcptun_restart
+				kill $(ps -ef|grep server_linux_amd64|grep -v grep|awk '{print $2}')
+				echo new > /var/log/kcptun.log
+			fi
+			if [ "$errorcondition3" ];then
 				echo $num
 				date >> /var/log/kcptun_restart.log
 				echo -n "  restarted server" >> /var/log/kcptun_restart
